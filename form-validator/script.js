5cddacf9551218c8fbfc68d54d00d03aefe71655
emmitorstensson <emmitorstensson@hotmail.com>
2020-11-13 16:21:07 +0100

    small project done

--- form-validator/script.js
+++ form-validator/script.js
@@ -20,24 +20,79 @@ function showSuccess(input) {
 
 function checkEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(email.value.trim())) {
+        showSuccess(email);
+    } else {
+        showError(email, 'Email is not valid');
+    }
+}
+
+// Check required fields
+function checkReq(inputArr) {
+    inputArr.forEach(function(input) {
+        if (input.value.trim() === '') {
+                showError(input, `${getFieldName(input)} is required`)
+        } else {
+            showSuccess(input)
+         }
+    });
+}
+
+// Get fieldname to make error msg start with uppercase
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
+// check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} is to short`)
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} is to long`)
+    }else {
+        showSuccess(input)
+    }
+}
+
+//check if password matches
+
+function checkPasswordMatch(input1, input2) {
+    if (input1.value !== input2.value ) {
+        showError(input2, 'Passwords do not match')
+    } else {
+        showSuccess(input1)
+        showSuccess(input2)
+    }
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
 
-    if (username.value === '') {
-        showError(username, "Username required")
-    } else {
-        showSuccess(username)
-    }
-
-    if (email.value === '') {
-        showError(email, "Email required")
-    } else if(!checkEmail(email.value)) {
-        showError(email, "Email is not valid")
-    } else {
-        showSuccess(email)
-    }
-   
+    checkReq([username, email, password, confirm_password])
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordMatch(password, confirm_password)
 })
+
+
+//      Works but messy
+
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault()
+
+//     if (username.value === '') {
+//         showError(username, "Username required")
+//     } else {
+//         showSuccess(username)
+//     }
+
+//     if (email.value === '') {
+//         showError(email, "Email required")
+//     } else if(!checkEmail(email.value)) {
+//         showError(email, "Email is not valid")
+//     } else {
+//         showSuccess(email)
+//     }
+   
+// })
