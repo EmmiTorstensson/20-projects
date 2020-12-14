ff47d5eb47cd3a62b95a79bc56c2bd772f88c8ff
emmitorstensson <emmitorstensson@hotmail.com>
2020-12-14 10:12:39 +0100

    added local storage, project done

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -6,14 +6,16 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-const dummyTransactions = [
-    {id: 1, text: 'Flowers', amount: -20 },
-    {id: 2, text: 'Salary', amount: 300 },
-    {id: 3, text: 'Books', amount: -10 },
-    {id: 4, text: 'Camera', amount: 150 },
-];
+// const dummyTransactions = [
+//     {id: 1, text: 'Flowers', amount: -20 },
+//     {id: 2, text: 'Salary', amount: 300 },
+//     {id: 3, text: 'Books', amount: -10 },
+//     {id: 4, text: 'Camera', amount: 150 },
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 // Add new transaction
 function addTransaction(e) {
@@ -33,6 +35,8 @@ function addTransaction(e) {
         addTransactionDOM(transaction);
 
         updateValues();
+
+        updateLocalStorage();
 
         text.value = '';
         amount.value = '';
@@ -87,7 +91,14 @@ function updateValues() {
 function removeTransaction(id) {
     transactions = transactions.filter(transaction => transaction.id !== id);
 
+    updateLocalStorage(); 
+
     init();
+}
+
+// Update localStorage transactions
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
 // Init app 
