a848b3ede008047dc38fd9f9703d1531a4e40146
emmitorstensson <emmitorstensson@hotmail.com>
2020-12-21 16:34:20 +0100

    added html

---
+++ infinite-scroll/script.js
@@ -0,0 +1,6 @@
+
+// End points
+
+// https://jsonplaceholder.typicode.com/posts?_limit=5 ---- limit post to 5
+
+// https://jsonplaceholder.typicode.com/posts?_limit=5&_page=2 ---- limit post to 5 but look at secong page (id: 6-10)
