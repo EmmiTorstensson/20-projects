c3e4324d34186414842bd73f202e14654f381c68
emmitorstensson <emmitorstensson@hotmail.com>
2021-01-19 21:28:50 +0100

    Set and get data from local storage

--- memory-cards/script.js
+++ memory-cards/script.js
@@ -17,20 +17,34 @@ let currentActiveCard = 0;
 const cardsEl = [];
 
 // Store card data 
-const cardsData = [
-    {
-        question: 'What must a variable begin with?',
-        answer: 'A letter, $ or _'
-    },
-    {
-        question: 'What is a variable?',
-        answer: 'Container for a piece of data'
-    },
-    {
-        question: 'Example of Case Sensitive Variable',
-        answer: 'thisIsAVariable'
-    }
-]
+const cardsData = getCardsData();
+
+    // const cardsData = [
+        // {
+        //     question: 'What must a variable begin with?',
+        //     answer: 'A letter, $ or _'
+        // },
+        // {
+        //     question: 'What is a variable?',
+        //     answer: 'Container for a piece of data'
+        // },
+        // {
+        //     question: 'Example of Case Sensitive Variable',
+        //     answer: 'thisIsAVariable'
+        // }
+    // ]
+
+// GEt cards data from local storage
+function getCardsData() {
+    const cards = JSON.parse(localStorage.getItem('cards'));
+    return cards === null ? [] : cards;
+}
+
+// Set card data to local storage
+function setCardsData(cards) {
+    localStorage.setItem('cards', JSON.stringify(cards))
+    window.location.reload();  
+}
 
 // Create all cards
 function createCards() {
@@ -103,3 +117,34 @@ prevBtn.addEventListener('click', () => {
     updateCurrentText();
 })
 
+
+// Event listener to add new card 
+showBtn.addEventListener('click', () => addContainer.classList.add('show'))
+hideBtn.addEventListener('click', () => addContainer.classList.remove('show'))
+
+// add new card
+addCardBtn.addEventListener('click', () => {
+    const question = questionEl.value
+    const answer = answerEl.value
+
+    if(question.trim() && answer.trim()) {
+        const newCard = { question, answer }
+
+        createCard(newCard);
+
+        questionEl.value = '';
+        answerEl.value = '';
+
+        addContainer.classList.remove('show');
+
+        cardsData.push(newCard);
+        setCardsData(cardsData);
+    }
+})
+
+// Clear cards
+clearBtn.addEventListener('click', () => {
+    localStorage.clear();
+    cardsContainer.innerHTML = '';
+    window.location.reload();
+});
